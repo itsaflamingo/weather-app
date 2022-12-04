1e7de138681d6cc152485fa347ce8a4c00f917ea
Liz <elizabeth.pinero.r@gmail.com>
2022-12-04 12:58:47 -0500

    Added default background & styles, changed choose background from if to switch statements, separated styles into new function.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ checkbox.addEventListener('change', (e) => {
 
 function search(e) {
     e.preventDefault()
-    let input = document.querySelector('input').value;
+    const input = document.querySelector('input').value;
     const error = document.querySelector('.error');
     const cityDiv = document.querySelector('.country').innerHTML;
 
@@ -21,7 +21,7 @@ function search(e) {
 
     if(input === null || input === undefined || input === "") {
         const city = cityDiv.split(' ').pop();
-        input = city
+        input = city;
     };
     
     fetchWeatherData(`https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=775f80930103eb948ca48290efbf90f1${unit}`).catch(err => {
@@ -71,6 +71,7 @@ const display = (data) => {
 
     const container = document.querySelector('.container');
     const icon = document.querySelector('.icon');
+    const tempIcon = document.querySelector('.temp-icon');
 
     const weatherDisplay = data.weather;
 
@@ -84,43 +85,46 @@ const display = (data) => {
     humidity.innerHTML = `Humidity: ${data.humidity}`;
     wind.innerHTML = `Wind: ${data.wind}`;
 
-    if(weatherDisplay === 'Clouds') {
-        container.style.backgroundImage = 'url(img/cloudy.jpg)';
-        container.style.backgroundColor = 'rgba(255,255,255,0.15)'
-        container.style.backgroundRepeat = 'no-repeat';
-        container.style.backgroundSize = 'cover';
-        container.style.backgroundBlendMode = 'lighten';
-        container.style.color = 'rgb(255, 255, 255)'
-
-        icon.style.backgroundImage = 'url(img/cloudy-day.png)';
-        icon.style.backgroundRepeat = 'no-repeat';
-        icon.style.backgroundSize = 'contain';
-    }
-    if(weatherDisplay === 'Clear') {
-        container.style.backgroundImage = 'url(img/sun.jpg)';
-        container.style.backgroundColor = 'rgba(255,255,255,0.15)'
-        container.style.backgroundRepeat = 'no-repeat';
-        container.style.backgroundSize = 'cover';
-        container.style.backgroundBlendMode = 'lighten';
-        container.style.color = 'rgba(63, 63, 63)'
-
-        icon.style.backgroundImage = 'url(img/sunny.png)';
-        icon.style.backgroundRepeat = 'no-repeat';
-        icon.style.backgroundSize = 'contain';
-    }
-
-    if(weatherDisplay === 'Rain' || weatherDisplay === 'Thunderstorm') {
-        container.style.backgroundImage = 'url(img/rainy.jpg)';
+    const changeContainerBackground = (url) => {
+        container.style.backgroundImage = url;
         container.style.backgroundColor = 'rgba(255,255,255,0.15)'
         container.style.backgroundRepeat = 'no-repeat';
         container.style.backgroundSize = '120% 100%';
         container.style.backgroundBlendMode = 'lighten';
         container.style.color = 'rgb(255, 255, 255)'
+    }
 
-        icon.style.backgroundImage = 'url(img/rain-512.png)';
+    const changeIcon = (weather, temp) => {
+        icon.style.backgroundImage = weather;
         icon.style.backgroundRepeat = 'no-repeat';
         icon.style.backgroundSize = 'contain';
 
+        tempIcon.style.backgroundImage = temp;
+        tempIcon.style.backgroundRepeat = 'no-repeat';
+        tempIcon.style.backgroundSize = 'contain';
+    }
+
+    switch(weatherDisplay) {
+        case 'Clouds': 
+            changeContainerBackground('url(img/cloudy.jpg)');
+            changeIcon('url(img/cloudy-day.png)', 'url(./img/temperature-512.png)');
+            break;
+        case 'Clear':
+            changeContainerBackground('url(img/sun.jpg)');
+            container.style.color = 'black'
+            changeIcon('url(img/sunny.png)', 'url(./img/black-temp.png)');
+            break;
+        case 'Rain' || 'Thunderstorm':
+            changeContainerBackground('url(img/rainy.jpg)');
+            changeIcon('url(img/rain-512.png)', 'url(./img/temperature-512.png)');
+            break;
+        case 'Mist':
+            changeContainerBackground('url(img/mist.jpg)');
+            changeIcon('url(img/misty.png)', 'url(./img/temperature-512.png)');
+            break;
+        default:
+            changeContainerBackground('url(img/default.jpg)');
+            changeIcon('url(img/sunny.png)', 'url(./img/temperature-512.png)');
     }
 }
 
